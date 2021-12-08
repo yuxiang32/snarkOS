ca6986fdb9ab9de12233fc67505184b094a1d27f
Zheng Yuxiang <yuxiang32@gmail.com>
2021-12-08 19:56:22 +0800

    Replace CRLF to LF

--- iptable.sh
+++ iptable.sh
@@ -3,12 +3,12 @@ iptables -F
 
 #sync nodes
 while read ip; do
-    iptables -A OUTPUT -p tcp -d $ip --dport 4132 -j ACCEPT
+    iptables -A OUTPUT -p tcp -d "$ip" --dport 4132 -j ACCEPT
 done <ips.txt
 
 #top nodes
 while read ip; do
-    iptables -A OUTPUT -p tcp -d $ip --dport 4132 -j ACCEPT
+    iptables -A OUTPUT -p tcp -d "$ip" --dport 4132 -j ACCEPT
 done <ip.txt
 
 iptables -A OUTPUT -p tcp --dport 4132 -j DROP
