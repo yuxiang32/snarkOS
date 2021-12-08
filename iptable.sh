78648243bbfdce78cf9847dc62fb25bf2e24002b
Zheng Yuxiang <yuxiang32@gmail.com>
2021-12-08 19:38:24 +0800

    Ip tables

---
+++ iptable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+iptables -F
+
+#sync nodes
+while read ip; do
+    iptables -A OUTPUT -p tcp -d $ip --dport 4132 -j ACCEPT
+done <ips.txt
+
+#top nodes
+while read ip; do
+    iptables -A OUTPUT -p tcp -d $ip --dport 4132 -j ACCEPT
+done <ip.txt
+
+iptables -A OUTPUT -p tcp --dport 4132 -j DROP
+iptables -L
